b3832a12f2060041d36ebc3b7848bde58d7f6f06
liroudev <liroudev@pm.me>
2021-01-20 21:28:08 -0300

    Add To Sum implementation / Adionado implementação de Somar

---
+++ Project-API/Controllers/CalculatorController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Project_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber )
+        {
+            
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string param)
+        {
+            var isNumber = false;
+            double numeroConvertido;
+
+           isNumber = double.TryParse(param, System.Globalization.NumberStyles.Any,
+                                System.Globalization.NumberFormatInfo.InvariantInfo,
+                                out numeroConvertido);
+
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string param)
+        {
+            decimal decimalValue;
+            
+            if (decimal.TryParse(param,out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+    }
+}
